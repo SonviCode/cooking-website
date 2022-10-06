030cbcc9a983ad10329af945159a51a983213d97
Tom Sonvico <tom.sonvico.pro@gmail.com>
2022-10-06 20:39:58 +0200

    clean code

--- src/pages/Meal.js
+++ src/pages/Meal.js
@@ -1,112 +1,38 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
+import { Link,  } from "react-router-dom";
+import { useLocation } from "react-router";
 import { v4 as uuidv4 } from "uuid";
 import Navbar from "../components/Navbar";
 
 const Meal = () => {
   const params = useParams();
 
+  const location = useLocation();
+
+  console.log(location.state);
+
+  let meal = location.state
+
   const [dataMeal, setDataMeal] = useState([]);
-
-  let handleParams;
-  switch (params.slug) {
-    case "Jam-Roly-Poly":
-      handleParams = params.slug.replace("-", " ");
-      break;
-    case "Home-made-Mandazi":
-    case "Chick-Fil-A-Sandwich":
-    case "Three-cheese-souffles":
-      handleParams =
-        params.slug.substring(0, params.slug.lastIndexOf("-")) +
-        " " +
-        params.slug.substring(params.slug.lastIndexOf("-") + 1);
-      break;
-    case "Piri-piri-chicken-and-slaw":
-    case "Soy-Glazed-Meatloaves-with-Wasabi-Mashed-Potatoes-&-Roasted-Carrots":
-      handleParams = params.slug.replaceAll("-", " ").replace(" ", "-");
-      break;
-    case "Vietnamese-Grilled-Pork-(bun-thit-nuong)":
-    case "Portuguese-prego-with-green-piri-piri":
-      handleParams = params.slug.replaceAll("-", " ").split(" ", 3).join(" ");
-
-      break;
-
-    default:
-      handleParams = params.slug.replaceAll("-", " ");
-  }
 
   useEffect(() => {
     axios
       .get(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${handleParams}`
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`
       )
       .then((res) => setDataMeal(res.data.meals));
   }, []);
 
-  let ingredient = [];
-  let measure = [];
-
-  if (dataMeal[0]) {
-    ingredient = [
-      dataMeal[0].strIngredient1,
-      dataMeal[0].strIngredient2,
-      dataMeal[0].strIngredient3,
-      dataMeal[0].strIngredient4,
-      dataMeal[0].strIngredient5,
-      dataMeal[0].strIngredient6,
-      dataMeal[0].strIngredient7,
-      dataMeal[0].strIngredient8,
-      dataMeal[0].strIngredient9,
-      dataMeal[0].strIngredient10,
-      dataMeal[0].strIngredient11,
-      dataMeal[0].strIngredient12,
-      dataMeal[0].strIngredient13,
-      dataMeal[0].strIngredient14,
-      dataMeal[0].strIngredient15,
-      dataMeal[0].strIngredient16,
-      dataMeal[0].strIngredient17,
-      dataMeal[0].strIngredient18,
-      dataMeal[0].strIngredient19,
-      dataMeal[0].strIngredient20,
-    ];
-  }
-
-  if (dataMeal[0]) {
-    measure = [
-      dataMeal[0].strMeasure1,
-      dataMeal[0].strMeasure2,
-      dataMeal[0].strMeasure3,
-      dataMeal[0].strMeasure4,
-      dataMeal[0].strMeasure5,
-      dataMeal[0].strMeasure6,
-      dataMeal[0].strMeasure7,
-      dataMeal[0].strMeasure8,
-      dataMeal[0].strMeasure9,
-      dataMeal[0].strMeasure10,
-      dataMeal[0].strMeasure11,
-      dataMeal[0].strMeasure12,
-      dataMeal[0].strMeasure13,
-      dataMeal[0].strMeasure14,
-      dataMeal[0].strMeasure15,
-      dataMeal[0].strMeasure16,
-      dataMeal[0].strMeasure17,
-      dataMeal[0].strMeasure18,
-      dataMeal[0].strMeasure19,
-      dataMeal[0].strMeasure20,
-    ];
-  }
   let number = "";
-  let Measures;
-  let Ingredients;
+  let measures = [];
+  let ingredients = [];
 
   for (let i = 1; i < 20; i++) {
-    number =+ i;
-    Ingredients = dataMeal[0] && dataMeal[0][`strIngredient${number}`]
-    Measures = dataMeal[0] && dataMeal[0][`strMeasure${number}`]
-    console.log(Ingredients);
-    console.log(Measures);
+    number = +i;
+    ingredients.push(dataMeal[0] && dataMeal[0][`strIngredient${number}`]);
+    measures.push(dataMeal[0] && dataMeal[0][`strMeasure${number}`]);
   }
 
   return (
@@ -161,10 +87,9 @@ const Meal = () => {
             <h2 className="text-2xl font-extrabold uppercase mb-10">
               Ingrédients :{" "}
             </h2>
-            <p>{Measures}</p>
             <div className="grid grid-cols-auto-fit100 gap-10 max-w-4xl mx-auto mb-20">
               {dataMeal[0] &&
-                ingredient.map(
+                ingredients.map(
                   (el) =>
                     el && (
                       <div
@@ -185,7 +110,7 @@ const Meal = () => {
                             {el}
                           </p>
                           <p key={uuidv4()}>
-                            {measure[`${ingredient.indexOf(el)}`]}
+                            {measures[`${ingredients.indexOf(el)}`]}
                           </p>
                         </Link>
                       </div>
